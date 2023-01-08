dbb3942d86a640c60ea2ff1499bf18190970af82
Alton D'souza <altondsouza02@gmail.com>
2023-01-08 16:03:04 +0530

    Functions

---
+++ function.py
@@ -0,0 +1,21 @@
+# Functions
+
+def test():
+    print("This is a function")
+
+
+# Function with parameters
+
+def sqft(w, h):
+    v = w * h
+    return v
+
+# Call a function
+test()
+print(sqft(12,3))
+
+# Call a function multiple Times
+for i in range(4):
+    test()
+
+# Functions and scope
